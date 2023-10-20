daf2b554222103227fe76a0b4fc21cfd5ee670cb
Michael Trazzi <michael.trazzi@gmail.com>
2023-10-20 14:00:00 +0200

    done with w0d1

--- w0d1.py
+++ w0d1.py
@@ -518,3 +518,58 @@ for i, case in enumerate(test_cases):
     else:
         print(f"Test {i} passed!")
 # %%
+
+
+def test_relu(relu_func):
+    print(f"Testing: {relu_func.__name__}")
+    x = t.arange(-1, 3, dtype=t.float32, requires_grad=True)
+    out = relu_func(x)
+    expected = t.tensor([0.0, 0.0, 1.0, 2.0])
+    assert_all_close(out, expected)
+
+
+# %%
+def relu_clone_setitem(x: t.Tensor) -> t.Tensor:
+    """Make a copy with torch.clone and then assign to parts of the copy."""
+    copy = t.clone(x)
+    copy[x < 0] = 0
+    return copy
+
+
+test_relu(relu_clone_setitem)
+
+
+# %%
+def relu_where(x: t.Tensor) -> t.Tensor:
+    """Use torch.where."""
+    return t.where(x > 0, x, 0.0)
+
+
+test_relu(relu_where)
+
+
+# %%
+def relu_maximum(x: t.Tensor) -> t.Tensor:
+    """Use torch.maximum."""
+    return t.maximum(x, t.zeros_like(x))
+
+
+test_relu(relu_maximum)
+
+
+# %%
+def relu_abs(x: t.Tensor) -> t.Tensor:
+    """Use torch.abs."""
+    return (x + t.abs(x)) / 2
+
+
+test_relu(relu_abs)
+
+
+# %%
+def relu_multiply_bool(x: t.Tensor) -> t.Tensor:
+    """Create a boolean tensor and multiply the input by it elementwise."""
+    return (x >= 0) * x
+
+
+test_relu(relu_multiply_bool)
