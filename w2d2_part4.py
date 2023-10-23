cb80aad9359d5304574c3026d59c7f26b5addb27
Michael Trazzi <michael.trazzi@gmail.com>
2023-10-23 14:22:33 +0200

    weight decay

--- w2d2_part4.py
+++ w2d2_part4.py
@@ -15,6 +15,8 @@ from tqdm.auto import tqdm
 import wandb
 from w2d1 import BertConfig, BertLanguageModel, predict
 from w2d2_part3 import cross_entropy_selected, random_mask
+from ipdb import set_trace as p
+from torchsummary import summary
 
 MAIN = __name__ == "__main__"
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
@@ -59,7 +61,7 @@ def lr_for_step(step: int, max_step: int, max_lr: float, warmup_step_frac: float
     if step <= warmup_steps:
         return 0.01 * max_lr + 0.99 * ((step / warmup_steps) * max_lr)
     return max_lr - ((step - warmup_steps) / (max_step - warmup_steps)) * 0.99 * max_lr
-    
+
 
 if MAIN:
     max_step = int(len(train_loader) * config_dict["epochs"]) # type: ignore
@@ -70,5 +72,43 @@ if MAIN:
     (fig, ax) = plt.subplots(figsize=(12, 4))
     ax.plot(lrs)
     ax.set(xlabel="Step", ylabel="Learning Rate", title="Learning Rate Schedule")
-    plt.plot()
-    plt.show()
+    # plt.plot()
+    # plt.show()
+
+def make_optimizer(model: BertLanguageModel, config_dict: dict) -> t.optim.AdamW:
+    """
+    Loop over model parameters and form two parameter groups:
+
+    - The first group includes the weights of each Linear layer and uses the weight decay in config_dict
+    - The second has all other parameters and uses weight decay of 0
+    """
+    linear_weights_ids = set()
+    count = 0
+    for name, module in model.named_modules():
+        if isinstance(module, nn.Linear):
+            linear_weights_ids.add(id(module.weight))
+
+    return t.optim.AdamW([
+        {'params': [p for p in model.parameters() if id(p) in linear_weights_ids], 'weight_decay': config_dict['weight_decay']},
+        {'params': [p for p in model.parameters() if id(p) not in linear_weights_ids]},
+    ], lr=config_dict['lr'], eps=config_dict['eps'])
+    
+
+
+if MAIN:
+    test_config = BertConfig(
+        max_position_embeddings=4, hidden_size=1, intermediate_size=4, num_layers=3, num_heads=1, head_size=1
+    )
+    optimizer_test_model = BertLanguageModel(test_config)
+    opt = make_optimizer(optimizer_test_model, dict(weight_decay=0.1, lr=0.0001, eps=1e-06))
+    expected_num_with_weight_decay = test_config.num_layers * 6 + 1
+    wd_group = opt.param_groups[0]
+    actual = len(wd_group["params"])
+    assert (
+        actual == expected_num_with_weight_decay
+    ), f"Expected 6 linear weights per layer (4 attn, 2 MLP) plus the final lm_linear weight to have weight decay, got {actual}"
+    all_params = set()
+    for group in opt.param_groups:
+        all_params.update(group["params"])
+    assert all_params == set(optimizer_test_model.parameters()), "Not all parameters were passed to optimizer!"
+
